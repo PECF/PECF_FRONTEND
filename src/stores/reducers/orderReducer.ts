04562f123cd41e60d102178351d09af3343fe47a
Thomi55 <thomyeze57@gmail.com>
2022-10-13 17:29:13 -0300

    order action y reducer

--- src/stores/reducers/orderReducer.ts
+++ src/stores/reducers/orderReducer.ts
@@ -19,10 +19,107 @@ export const newOrderReducer = (state = {}, action) => {
   };
 };
 
-export const myOrdersReducer = (state = { orders: [] }, action) => {};
+export const myOrdersReducer = (state = { orders: [] }, action) => {
+  const reducers = {
+    MY_ORDER_REQUEST: {
+      loading: true,
+    },
+    MY_ORDER_SUCCESS: {
+      loading: false,
+      orders: action.payload,
+    },
+    MY_ORDERS_FAIL: {
+      loading: false,
+      error: action.payload,
+    },
+    CLEAR_ERRORS: {
+      ...state,
+      error: null,
+    },
+  };
+};
 
-export const allOrdersReducer = (state = { orders: [] }, action) => {};
+export const allOrdersReducer = (state = { orders: [] }, action) => {
+  const reducers = {
+    ALL_ORDERS_REQUEST: {
+      loading: true,
+    },
+    ALL_ORDERS_SUCCESS: {
+      loading: false,
+      orders: action.payload,
+    },
+    ALL_ORDERS_FAIL: {
+      loading: false,
+      error: action.payload,
+    },
+    CLEAR_ERRORS: {
+      ...state,
+      error: null,
+    },
+  };
+};
 
-export const orderReducer = (state = {}, action) => {};
+export const orderReducer = (state = {}, action) => {
+  const reducers = {
+    UPDATE_ORDER_REQUEST: {
+      ...state,
+      loading: true,
+    },
+    DELETE_ORDER_REQUEST: {
+      ...state,
+      loading: true,
+    },
+    UPDATE_ORDER_SUCCESS: {
+      ...state,
+      loading: false,
+      isUpdated: action.payload,
+    },
+    DELETE_ORDER_SUCCESS: {
+      ...state,
+      loading: false,
+      isDeleted: action.payload,
+    },
+    UPDATE_ORDER_FAIL: {
+      ...state,
+      loading: false,
+      error: action.payload,
+    },
+    DELETE_ORDER_FAIL: {
+      ...state,
+      loading: false,
+      error: action.payload,
+    },
+    UPDATE_ORDER_RESET: {
+      ...state,
+      isUpdated: false,
+    },
+    DELETE_ORDER_RESET: {
+      ...state,
+      isDeleted: false,
+    },
+    CLEAR_ERRORS: {
+      ...state,
+      error: null,
+    },
+  };
+};
 
-export const orderDetailsReducer = (state = { order: {} }, action) => {};
+export const orderDetailsReducer = (state = { order: {} }, action) => {
+  const request = {
+    ORDER_DETAILS_REQUEST: {
+      loading: true,
+    },
+    ORDER_DETAILS_SUCCESS: {
+      loading: false,
+      order: action.payload,
+    },
+    ORDER_DETAILS_FAIL: {
+      loading: false,
+      error: action.payload,
+    },
+    CLEAR_ERRORS: {
+      ...state,
+      error: null,
+    },
+  };
+};
