0ac454055e6c19c0ef9ffce2e05dc5dae8859a76
JoaquinDW <balthasardeweert@gmail.com>
2022-10-15 17:08:22 -0300

    NavBar with modal for login and sing up

--- src/views/NavBar/Navbar.tsx
+++ src/views/NavBar/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { SearchIcon } from "@chakra-ui/icons";
-import "./App.css";
 import {
   Box,
   InputGroup,
@@ -12,9 +11,22 @@ import {
   Heading,
   Button,
   Flex,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  FormLabel,
+  FormControl,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 
 function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Container maxWidth="100%">
       <Flex
@@ -43,14 +55,72 @@ function NavBar() {
         <Spacer />
 
         <ButtonGroup gap="1">
-          <Button colorScheme="teal" variant="outline">
+          <Button colorScheme="teal" variant="outline" onClick={onOpen}>
             Sign Up
           </Button>
 
-          <Button colorScheme="teal" variant="outline">
+          <Button colorScheme="teal" variant="outline" onClick={onOpen}>
             Log in
           </Button>
         </ButtonGroup>
+
+        <Modal isOpen={isOpen} onClose={onClose} size="md" gap={10}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Log In</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Enter your email</FormLabel>
+                <Input placeholder="email@example.com" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button variant="ghost">Forgot your password?</Button>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Log In
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        <Modal isOpen={isOpen} onClose={onClose} size="md" gap={10}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Sing in</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Enter your email</FormLabel>
+                <Input placeholder="email@example.com" />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Enter your full Name</FormLabel>
+                <Input placeholder="Juan Perez" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button variant="ghost">Forgot your password?</Button>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Log In
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
         <Button colorScheme="teal" size="md" padding="5" width="60">
           Cart
         </Button>
