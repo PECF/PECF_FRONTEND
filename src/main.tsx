9ca21f09e14d02289e170880e77d97f14ddb4411
Syrident23 <santiagodabbene@gmail.com>
2022-10-18 11:54:45 -0300

    Headers and Routes Fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,14 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./stores/rootStore";
+import ReactDOM from "react-dom";
+import * as React from "react";
 import App from "./App";
 import "./index.css";
-import store from "./stores/rootStore";
-import { ChakraProvider } from "@chakra-ui/react";
 
 
-
-// import { positions, transitions, Provider as AlertProvider } from "react-alert";
-// import AlertTemplate from "react-alert-template-basic";
-
-// const options = {
-//   timeout: 5000,
-//   position: positions.BOTTOM_CENTER,
-//   transition: transitions.SCALE,
-// };
 
 const el = document.getElementById('root')
 if (el === null) throw new Error('Root container missing in index.html')
@@ -31,9 +21,7 @@ root.render(
   <BrowserRouter>
     <Provider store={store}>
       <ChakraProvider>
-        {/* <AlertProvider template={AlertTemplate} {...options}> */}
         <App />
-        {/* </AlertProvider> */}
       </ChakraProvider>
     </Provider>
   </BrowserRouter>
