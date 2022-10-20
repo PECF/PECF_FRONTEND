682d3c569824d0710d0bb7102c0ccd76f0aa3ab6
Syrident23 <santiagodabbene@gmail.com>
2022-10-20 20:15:41 -0300

    Home Styles Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,8 @@ import { Card } from "../components/Card";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import SliderCard from '../components/SliderCard'
-
+import { Link } from "react-router-dom";
+import { Text } from '@chakra-ui/react';
 import {
   Box,
   Container,
@@ -13,6 +14,7 @@ import {
   Flex,
   IconButton,
   useBreakpointValue,
+  Button,
 } from "@chakra-ui/react";
 
 
@@ -35,7 +37,7 @@ const settings2 = {
       slidesToShow: 5,
       centerMode: true,
       centerPadding: "10px",
-      slidesToScroll: 5,
+      slidesToScroll: 1,
       
       initialSlide: 3,
       
@@ -44,7 +46,7 @@ const settings2 = {
           breakpoint: 1300,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 2,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
@@ -66,6 +68,7 @@ const settings2 = {
         }
       ]
 };
+
 
 
 export function Home() {
@@ -152,14 +155,131 @@ export function Home() {
             ))}
           </Slider>
         </Box>
-        <Box width={'35%'} height={'25vh'} bgSize='cover' bgImg={'https://media.istockphoto.com/photos/mens-suits-on-hangers-in-different-colors-picture-id887360960?k=20&m=887360960&s=612x612&w=0&h=N0k2yX9noV6kNgpmKbcSXaLOl2x8Mbt_lyppAmfhNcA='} >
-            
+        <Box 
+        role={'group'}
+        width={'35%'} 
+        height={'25vh'} 
+        bgSize='cover'
+        display={'flex'}
+        justifyContent='center'
+        alignItems={'center'}
+        bgImg={'https://media.istockphoto.com/photos/mens-suits-on-hangers-in-different-colors-picture-id887360960?k=20&m=887360960&s=612x612&w=0&h=N0k2yX9noV6kNgpmKbcSXaLOl2x8Mbt_lyppAmfhNcA='} 
+        
+        >
+          <Box 
+          visibility={'hidden'}
+          backgroundColor='gray.300'
+          opacity={'0'}
+          boxSize={'100%'}
+          display={'flex'} 
+          flexDir='column'
+          justifyContent='center'
+          alignItems={'center'}
+          _groupHover={{
+            visibility: 'visible',
+            opacity: '0.7'
+          }}
+          >
+              
+              <Text as='b' color={'black'} _hover={{opacity:1}}>Men Clothes</Text>
+              <Button as={Link} to={''} _hover={{opacity:1, backgroundColor:'white' }} variant={'outline'}>See more</Button>
+          
+          </Box>
         </Box>
-        <Box width={'35%'} height={'25vh'} bgSize='cover' bgImg='https://media.istockphoto.com/photos/polka-dot-summer-brown-dress-suede-wedge-sandals-eco-straw-tote-bag-picture-id1208148708?k=20&m=1208148708&s=612x612&w=0&h=rjZiAPCOpwREiTET21lTP3wM30BUqAG9PjocC-euJ98=' >
+        <Box 
+        role={'group'}
+        width={'35%'} 
+        height={'25vh'} 
+        bgSize='cover'
+        display={'flex'}
+        justifyContent='center'
+        alignItems={'center'}
+        bgImg='https://media.istockphoto.com/photos/polka-dot-summer-brown-dress-suede-wedge-sandals-eco-straw-tote-bag-picture-id1208148708?k=20&m=1208148708&s=612x612&w=0&h=rjZiAPCOpwREiTET21lTP3wM30BUqAG9PjocC-euJ98=' 
+
+        >
+        <Box 
+          visibility={'hidden'}
+          backgroundColor='gray.300'
+          opacity={'0'}
+          boxSize={'100%'}
+          display={'flex'} 
+          flexDir='column'
+          justifyContent='center'
+          alignItems={'center'}
+          _groupHover={{
+            visibility: 'visible',
+            opacity: '0.7'
+          }}
+          >
+              
+              <Text as='b' color={'black'} _hover={{opacity:1}}>Women Clothes</Text>
+              <Button as={Link} to={''} _hover={{opacity:1, backgroundColor:'white' }} variant={'outline'}>
+              See More
+              </Button>
+          
+          </Box>
         </Box>
-        <Box width={'35%'} height={'25vh'} bgSize='cover' bgImg='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzorsnZK_FNGOBVuxZT95bfnOhtQ1bPCU8kxlumfHi9g_NCZlFCvR81o3JcTZ59Ndr__o&usqp=CAU' >
+        <Box 
+        role={'group'}
+        width={'35%'} 
+        height={'25vh'} 
+        bgSize='cover'
+        display={'flex'}
+        justifyContent='center'
+        alignItems={'center'}
+        bgImg='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzorsnZK_FNGOBVuxZT95bfnOhtQ1bPCU8kxlumfHi9g_NCZlFCvR81o3JcTZ59Ndr__o&usqp=CAU' 
+        
+        >
+        <Box 
+          visibility={'hidden'}
+          backgroundColor='gray.300'
+          opacity={'0'}
+          boxSize={'100%'}
+          display={'flex'} 
+          flexDir='column'
+          justifyContent='center'
+          alignItems={'center'}
+          _groupHover={{
+            visibility: 'visible',
+            opacity: '0.7'
+          }}
+          >
+              
+              <Text as='b' color={'black'} _hover={{opacity:1}}>Child Clothes</Text>
+              <Button as={Link} to={''} _hover={{opacity:1, backgroundColor:'white' }} variant={'outline'}>See more</Button>
+          
+          </Box>
         </Box>
-        <Box width={'35%'} height={'25vh'} bgSize='cover' bgImg='https://img.freepik.com/free-photo/workplace-business-modern-male-accessories-laptop-black-background_155003-3944.jpg?w=2000' >
+        <Box 
+        role={'group'}
+        width={'35%'} 
+        height={'25vh'} 
+        bgSize='cover'
+        display={'flex'}
+        justifyContent='center'
+        alignItems={'center'}
+        bgImg='https://img.freepik.com/free-photo/workplace-business-modern-male-accessories-laptop-black-background_155003-3944.jpg?w=2000' 
+      
+        >
+        <Box 
+          visibility={'hidden'}
+          backgroundColor='gray.300'
+          opacity={'0'}
+          boxSize={'100%'}
+          display={'flex'} 
+          flexDir='column'
+          justifyContent='center'
+          alignItems={'center'}
+          _groupHover={{
+            visibility: 'visible',
+            opacity: '0.7'
+          }}
+          >
+              
+              <Text as='b' color={'black'} _hover={{opacity:1}}>Accesories</Text>
+              <Button as={Link} to={''} _hover={{opacity:1, backgroundColor:'white' }} variant={'outline'}>See more</Button>
+          
+          </Box>
         </Box>
       </Flex>
     
