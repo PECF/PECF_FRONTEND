6e8e58e09857e7e3f036df4250c25089ce64906e
FerMortaro <mortarofernanda@gmail.com>
2022-10-20 22:55:04 -0300

    my orders with test file

---
+++ src/views/myOrders/orders.ts
@@ -0,0 +1,29 @@
+export const orders = [
+  {
+    id: 1,
+    name: "remera",
+    image:
+      "https://d2r9epyceweg5n.cloudfront.net/stores/001/205/102/products/remera-lisa-vi-rj1-122009f0e7fe0bfa3715906956218434-480-0.jpg",
+    description: "remera lisa color verde de una tela muy suave y bonita.",
+    price: 250,
+    state: "delivered",
+  },
+  {
+    id: 2,
+    name: "remera",
+    image:
+      "https://d2r9epyceweg5n.cloudfront.net/stores/001/205/102/products/remera-lisa-vi-rj1-122009f0e7fe0bfa3715906956218434-480-0.jpg",
+    description: "remera lisa color verde de una tela muy suave y bonita.",
+    price: 250,
+    state: "delivered",
+  },
+  {
+    id: 3,
+    name: "remera",
+    image:
+      "https://d2r9epyceweg5n.cloudfront.net/stores/001/205/102/products/remera-lisa-vi-rj1-122009f0e7fe0bfa3715906956218434-480-0.jpg",
+    description: "remera lisa color verde de una tela muy suave y bonita.",
+    price: 250,
+    state: "delivered",
+  },
+];
