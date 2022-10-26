9dd2665efd86b78f160810d445de6520ac7cab53
Nacho-Calas <nachocalas@hotmail.com>
2022-10-26 17:58:18 -0300

    DetailProdut, carousel

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import {
   Box,
-  Container,
   Stack,
   Text,
   Image,
-  Flex,
   VStack,
   Button,
   Heading,
@@ -14,61 +12,56 @@ import {
   useColorModeValue,
   List,
   ListItem,
+  HStack,
+  Center,
+  Badge,
 } from "@chakra-ui/react";
 import { MdLocalShipping } from "react-icons/md";
+import { useRecoveryData } from "../hooks/useRecoveryData";
+import { CarouselDetailProducts } from "../components/carouselDetailProduct";
+
+
 
 export default function ProductDetail() {
+
+  const { products } = useRecoveryData("productList") 
+
   return (
-    <VStack>
-      <Box
+    <Center>
+      <VStack
         mt={10}
-        maxW="746px"
+        maxW="5xl"
         bg={"whiteAlpha.100"}
-        borderWidth="2px"
-        borderRadius="lg"
-        overflow="hidden"
-        p={50}
-        boxShadow="lg"
+        p={5}
       >
-        <Container maxW={"7xl"}>
-          <SimpleGrid
-            columns={{ base: 1, lg: 2 }}
-            spacing={{ base: 8, md: 10 }}
-            py={{ base: 18, md: 24 }}
-          >
-            <Flex>
-              <Image
-                rounded={"md"}
-                alt={"product image"}
-                src={
-                  "https://images.unsplash.com/photo-1596516109370-29001ec8ec36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODE1MDl8MHwxfGFsbHx8fHx8fHx8fDE2Mzg5MzY2MzE&ixlib=rb-1.2.1&q=80&w=1080"
-                }
-                fit={"cover"}
-                align={"center"}
-                w={"100%"}
-                h={{ base: "100%", sm: "400px", lg: "500px" }}
-              />
-            </Flex>
-            <Stack spacing={{ base: 6, md: 10 }}>
+        <Stack >
+            <Stack>
               <Box as={"header"}>
-                <Heading
-                  lineHeight={1.1}
-                  fontWeight={600}
-                  fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
+                                                              {/* //!TITLEE */}
+                <Heading                                       
+                  fontWeight={400}
+                  fontSize={{ base: "2xl", sm: "3xl", lg: "4xl" }}
                 >
                   Automatic Watch
                 </Heading>
-                <Text
-                  color={useColorModeValue("gray.900", "gray.400")}
-                  fontWeight={300}
-                  fontSize={"2xl"}
-                >
-                  $350.00 USD
-                </Text>
-              </Box>
+                </Box>
+                
+                <CarouselDetailProducts
+                  array = {products[0]?.image}/>
+              {/*    rounded={"md"}
+                alt={"product image"}
 
-              <Stack
-                spacing={{ base: 4, sm: 6 }}
+                objectFit={"cover"}
+                align={"center"}
+                w={{ base: "-moz-initial",  sm: "700px", lg: '700px'}}
+                h={{ base: "-moz-initial",  sm: "700px", lg: '700px'}}  */}
+                
+            </Stack>
+            <VStack spacing={{ base: 6, md: 10 }}
+            p={5}>
+                                                      {/* //!DIVISOR LINEA */}
+              <Stack                                       
+                spacing={{ base: 3, sm: 5 }}
                 direction={"column"}
                 divider={
                   <StackDivider
@@ -76,26 +69,26 @@ export default function ProductDetail() {
                   />
                 }
               >
-                <VStack spacing={{ base: 4, sm: 6 }}>
+                <Stack spacing={{ base: 4, sm: 6 }}>  {/* //!TEXTO DESCRIPCION */}
                   <Text
                     color={useColorModeValue("gray.500", "gray.400")}
-                    fontSize={"2xl"}
-                    fontWeight={"300"}
+                    fontSize={{ base: "-moz-initial", sm:"xl", lg:"2xl"}}
+                    fontWeight={"300"} align={'justify'}
                   >
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                     diam nonumy eirmod tempor invidunt ut labore
                   </Text>
-                  <Text fontSize={"lg"}>
+                  <Text align={'justify'} fontSize={"lg"}>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
                     aliquid amet at delectus doloribus dolorum expedita hic,
                     ipsum maxime modi nam officiis porro, quae, quisquam quos
                     reprehenderit velit? Natus, totam.
                   </Text>
-                </VStack>
+                </Stack>
                 <Box>
                   <Text
-                    fontSize={{ base: "16px", lg: "18px" }}
-                    color={useColorModeValue("yellow.500", "yellow.300")}
+                   fontSize={{ base: "-moz-initial", sm:"xl", lg:"2xl"}}
+                    color={useColorModeValue("teal.500", "teal.300")}
                     fontWeight={"500"}
                     textTransform={"uppercase"}
                     mb={"4"}
@@ -118,6 +111,13 @@ export default function ProductDetail() {
                 </Box>
                 
               </Stack>
+                <Text
+                  color={useColorModeValue("gray.900", "gray.400")}
+                  fontWeight={300}
+                  fontSize={"2xl"}
+                >
+                  $350.00 USD
+                </Text>
 
               <Button
                 rounded={"none"}
@@ -144,10 +144,10 @@ export default function ProductDetail() {
                 <MdLocalShipping />
                 <Text>2-3 business days delivery</Text>
               </Stack>
-            </Stack>
-          </SimpleGrid>
-        </Container>
-      </Box>
-    </VStack>
+            </VStack>
+          
+        </Stack>
+      </VStack>
+    </Center>
   );
 }
