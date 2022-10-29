d0533856a73f3d8dfebe76765ef9d704c19aa927
FerMortaro <mortarofernanda@gmail.com>
2022-10-29 16:54:41 -0300

    menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -87,7 +87,9 @@ export default function Acordion() {
                   colorScheme="teal"
                   variant="ghost"
                   size="md"
-                  onClick={toggleColorMode} aria-label={""}                ></IconButton>
+                  onClick={toggleColorMode}
+                  aria-label={""}
+                ></IconButton>
               </MenuItem>
             </MenuList>
             {/* <Button
@@ -142,7 +144,9 @@ export default function Acordion() {
                   colorScheme="teal"
                   variant="ghost"
                   size="sm"
-                  onClick={toggleColorMode} aria-label={""}                ></IconButton>
+                  onClick={toggleColorMode}
+                  aria-label={""}
+                ></IconButton>
                 <Text>{text}</Text>
               </MenuItem>
             </MenuList>
