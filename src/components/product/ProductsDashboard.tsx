874c2df91b3152d02d3ccbec39701d4347210e78
nahuelRosas <nahuel.rosas21@gmail.com>
2022-10-31 16:06:28 -0300

    product dashboard

--- src/components/product/ProductsDashboard.tsx
+++ src/components/product/ProductsDashboard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, SetStateAction } from "react";
+import { Select } from "chakra-react-select";
 import { SearchIcon } from "@chakra-ui/icons";
 import {
   Box,
@@ -28,6 +29,7 @@ import {
   ModalCloseButton,
   Button,
   useDisclosure,
+  Grid,
 } from "@chakra-ui/react";
 
 import { useRecoveryData } from "../../hooks/useRecoveryData";
@@ -36,9 +38,11 @@ export function ProductsDashboard() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   //searchbar filter function for products
-  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [contextSearch, setContextSearch] = useState(0);
+
   const handleChange = (event: {
-    target: { value: React.SetStateAction<string> };
+    target: { value: SetStateAction<string> };
   }) => {
     setSearchTerm(event.target.value);
   };
@@ -53,16 +57,94 @@ export function ProductsDashboard() {
           category: string;
           stock: number;
           _id: number;
+          createdAt: string;
         }) => {
-          const _filter = searchTerm.toLowerCase();
-          return (
-            product.name.toLowerCase().includes(_filter) ||
-            product.description.toLowerCase().includes(_filter) ||
-            product.price.toString().toLowerCase().includes(_filter) ||
-            product.category.toLowerCase().includes(_filter) ||
-            product.stock.toString().toLowerCase().includes(_filter) ||
-            product._id.toString().toLowerCase().includes(_filter)
-          );
+          const _filter = searchTerm
+            .toLowerCase()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "");
+          return contextSearch === 0
+            ? product.name
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter) ||
+                product.description
+                  .toLowerCase()
+                  .normalize("NFD")
+                  .replace(/[\u0300-\u036f]/g, "")
+                  .includes(_filter) ||
+                product.price
+                  .toString()
+                  .normalize("NFD")
+                  .replace(/[\u0300-\u036f]/g, "")
+                  .toLowerCase()
+                  .includes(_filter) ||
+                product.category
+                  .toString()
+                  .toLowerCase()
+                  .normalize("NFD")
+                  .replace(/[\u0300-\u036f]/g, "")
+                  .includes(_filter) ||
+                product.stock
+                  .toString()
+                  .toLowerCase()
+                  .normalize("NFD")
+                  .replace(/[\u0300-\u036f]/g, "")
+                  .includes(_filter) ||
+                product._id
+                  .toString()
+                  .toLowerCase()
+                  .normalize("NFD")
+                  .replace(/[\u0300-\u036f]/g, "")
+                  .includes(_filter)
+            : contextSearch === 1
+            ? product.name
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter)
+            : contextSearch === 2
+            ? product._id
+                .toString()
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter)
+            : contextSearch === 3
+            ? product.category
+                .toString()
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter)
+            : contextSearch === 4
+            ? product.price
+                .toString()
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter)
+            : contextSearch === 5
+            ? product.stock
+                .toString()
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter)
+            : contextSearch === 6
+            ? product.description
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter)
+            : contextSearch === 7
+            ? new Date(product.createdAt)
+                .toLocaleString()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(_filter)
+            : null;
         }
       );
 
@@ -90,9 +172,9 @@ export function ProductsDashboard() {
             Products
           </Heading>
         </Flex>
-        <Flex
-          justify="space-between"
-          align="center"
+        <Grid
+          templateColumns="2fr 1fr"
+          gap={6}
           px={6}
           py={4}
           bg={useColorModeValue("gray.50", "gray.800")}
@@ -109,9 +191,24 @@ export function ProductsDashboard() {
               <SearchIcon color="gray.300" />
             </InputRightElement>
           </InputGroup>
-        </Flex>
+          <Select
+            placeholder="Select Filter"
+            defaultValue={{ value: 0, label: "All" }}
+            options={[
+              { value: 0, label: "All" },
+              { value: 1, label: "Name" },
+              { value: 2, label: "ID" },
+              { value: 3, label: "Category" },
+              { value: 4, label: "Price" },
+              { value: 5, label: "Stock" },
+              { value: 6, label: "Description" },
+              { value: 7, label: "Date" },
+            ]}
+            onChange={(e) => (e ? setContextSearch(e.value) : 0)}
+          />
+        </Grid>
         <TableContainer>
-          <Table variant="simple">
+          <Table variant="striped" colorScheme="teal">
             <TableCaption>Products</TableCaption>
             <Thead>
               <Tr>
@@ -158,140 +255,4 @@ export function ProductsDashboard() {
       </VStack>
     </Box>
   );
-}
-
-//   return (
-//     <Box
-//       as="form"
-//       w="full"
-//       mt={8}
-//       maxW="full"
-//       mx="auto"
-//       bg={useColorModeValue("white", "gray.700")}
-//       overflow="hidden">
-//       <VStack align="stretch" spacing={0}>
-//         <Flex
-//           justify="space-between"
-//           align="center"
-//           px={6}
-//           py={4}
-//           bg={useColorModeValue("gray.50", "gray.800")}
-//           borderBottomWidth="1px">
-//           <Heading
-//             size="lg"
-//             fontWeight="bold"
-//             color={useColorModeValue("gray.900", "white")}>
-//             Products
-//           </Heading>
-//         </Flex>
-//         <Flex
-//           justify="space-between"
-//           align="center"
-//           px={6}
-//           py={4}
-//           bg={useColorModeValue("gray.50", "gray.800")}
-//           borderBottomWidth="1px">
-//           <InputGroup>
-//             <Input
-//               placeholder="Search"
-//               variant="filled"
-//               _placeholder={{ color: "gray.500" }}
-//             />
-//             <InputRightElement pointerEvents="none">
-//               <SearchIcon color="gray.300" />
-//             </InputRightElement>
-//           </InputGroup>
-//         </Flex>
-//       </VStack>
-//     </Box>
-//   );
-// }
-
-{
-  /* <VStack align="stretch" spacing={0}>
-        <Flex
-          justify="space-between"
-          align="center"
-          px={6}
-          py={4}
-          bg={useColorModeValue("gray.50", "gray.800")}
-          borderBottomWidth="1px">
-          <Heading
-            size="lg"
-            fontWeight="bold"
-            color={useColorModeValue("gray.900", "white")}>
-            Products
-          </Heading>
-        </Flex>
-        <Flex
-          justify="space-between"
-          align="center"
-          px={6}
-          py={4}
-          bg={useColorModeValue("gray.50", "gray.700")}
-          borderBottomWidth="1px">
-          <InputGroup size="md">
-            <Input
-              pr="4.5rem"
-              type="text"
-              placeholder="Search"
-              aria-label="Search"
-              bg={useColorModeValue("gray.100", "gray.800")}
-            />
-            <InputRightElement width="4.5rem">
-              <SearchIcon color="gray.300" />
-            </InputRightElement>
-          </InputGroup>
-        </Flex>
-
-        <TableContainer>
-          <Table variant="striped" colorScheme="teal">
-            <TableCaption>Products</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>Product</Th>
-                <Th>Price</Th>
-                <Th>Stock</Th>
-                <Th>Rating</Th>
-                <Th>ID</Th>
-                <Th>Date Relased</Th>
-                <Th>Active</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {products.map((product: any) => (
-                <Tr key={product._id} onClick={onOpen}>
-                  <Td>{product.name}</Td>
-                  <Td>{product.price}</Td>
-                  <Td>{product.stock}</Td>
-                  <Td>{product.rating}</Td>
-                  <Td>{product._id}</Td>
-                  <Td>{new Date(product?.createdAt).toLocaleString()}</Td>
-                  <Td>
-                    <Switch
-                      colorScheme="teal"
-                      defaultChecked={product.visibility}
-                    />
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-            <Tfoot>
-              <Tr>
-                <Th>Product</Th>
-                <Th>Price</Th>
-                <Th>Stock</Th>
-                <Th>Rating</Th>
-                <Th>ID</Th>
-                <Th>Date Relased</Th>
-                <Th>Active</Th>
-              </Tr>
-            </Tfoot>
-          </Table>
-        </TableContainer>
-      </VStack>
-      
-    </Box>
-  );
-} */
 }
