ab117845a8d7ec14bcef7f282964f82ca78758e4
nahuelRosas <nahuel.rosas21@gmail.com>
2022-10-31 18:29:34 -0300

    fix update product and connect with dashboard

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,12 +4,7 @@ import { PrivateRoute } from "./PrivateRoute";
 import { AdminRoute } from "./AdminRoute";
 import { Home } from "../pages/Home";
 import { Refund } from "../pages/Refund";
-// import { UpdatePassword } from "../pages/user/UpdatePassword";
-// import { Login } from "../pages/Login";
-// import { Register } from "../pages/Register";
 import { ForgotPassword } from "../pages/user/ForgotPassword";
-// import { UpdateProfile } from "../pages/UpdateProfile";
-// import { Admin } from "../pages/Admin";
 import { NotFound } from "../pages/NotFound";
 import { CreateProduct } from "../components/CreateProduct";
 import { useRecoveryData } from "../hooks/useRecoveryData";
@@ -18,7 +13,6 @@ import { ProductReview } from "../components/ProductReview";
 import { Products } from "../pages/Products";
 import { ProductDetail } from "../pages/ProductDetail";
 
-// import AuthContext from "../contexts/authContext";
 export const AppRoutes: React.FC = () => {
   const { user } = useRecoveryData("userDetails");
   return (
@@ -65,12 +59,23 @@ export const AppRoutes: React.FC = () => {
       )}
       {user?.role === "admin" ? (
         <Route
-          path="/profile/admin/updateProduct"
+          path="/profile/admin/updateProduct/:id"
           element={<Profile index={8} />}
         />
       ) : (
         <Route
-          path="/profile/admin/updateProduct"
+          path="/profile/admin/updateProduct/:id"
+          element={<Navigate to="/" />}
+        />
+      )}
+      {user?.role === "admin" ? (
+        <Route
+          path="/profile/admin/updateProduct/"
+          element={<Profile index={8} />}
+        />
+      ) : (
+        <Route
+          path="/profile/admin/updateProduct/"
           element={<Navigate to="/" />}
         />
       )}
@@ -84,48 +89,3 @@ export const AppRoutes: React.FC = () => {
     </Routes>
   );
 };
-
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="forgot-password" element={<ForgotPassword />} />
-//       <Route path="refund" element={<Refund />} />
-//       <Route path="create-product" element={<CreateProduct />} />
-//       <Route path="profile" element={<Profile />} />
-//       <Route path="*" element={<NotFound />} />
-//     </Routes>
-//   );
-// };
-
-// return (
-//   <Routes>
-//     <Route path="/" element={<Home />} />
-
-//     {/* <Route path="/login" element={<Login />} />
-//     <Route path="/register" element={<Register />} />*/}
-
-//     {!userInfo ? (
-//       <Route path="/forgotpassword" element={<ForgotPassword />} />
-//     ) : (
-//       <Route path="/forgotpassword" element={<Navigate to="/" />} />
-//     )}
-
-//     {/* {user && user.role === "admin" ? (
-//       <Route path="/createproduct" element={<CreateProduct />} />
-//     ) : (
-//       <Route path="/createproduct" element={<Navigate to="/" />} />
-//     )} */}
-
-//     <Route path="/createproduct" element={<CreateProduct />} />
-
-//     {/* {user && user.role === "admin" ? (
-//     ) : (
-//       <Route path="/createproduct" element={<Navigate to="/" />} />
-//     )} */}
-
-//     {/* <PrivateRoute path="/update-profile" element={<UpdateProfile />} />
-//     <AdminRoute path="/admin" element={<Admin />} />  */}
-//     <Route path="*" element={<NotFound />} />
-//   </Routes>
-// );
-// };
