6f36832bf90e2f47c612da1e21d5f0f6bd7eac73
FerMortaro <mortarofernanda@gmail.com>
2022-11-03 01:22:07 -0300

    margenes al carrito/checkout

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -43,13 +43,13 @@ export default function Cart() {
 
   const [checkOut, setCheckOut] = React.useState(false);
 
-  const [name , setName] = React.useState("");
-  const [shippingOption , setShippingOption] = React.useState("");
-  const [address , setAddress] = React.useState("");
-  const [city , setCity] = React.useState("");
-  const [postalCode , setPostalCode] = React.useState("");
-  const [paymentMethod , setPaymentMethod] = React.useState("");
-  const [comment , setComment] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [shippingOption, setShippingOption] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [postalCode, setPostalCode] = React.useState("");
+  const [paymentMethod, setPaymentMethod] = React.useState("");
+  const [comment, setComment] = React.useState("");
 
   return (
     <Box onClick={onOpen}>
@@ -146,39 +146,69 @@ export default function Cart() {
                       size="md"
                       onClick={onClose}
                     />
-                    <Heading as={"h3"} fontSize="2xl" fontWeight="bold">
+                    <Heading as={"h3"} fontSize="2xl" fontWeight="bold" mb={10}>
                       Complete your Order ({cartItems.length})
                     </Heading>
                     <FormControl id="Name">
                       <FormLabel>Please enter your Full Name</FormLabel>
-                      <Input type="text" onChange={(e)=> setName(e.target.value)} value={name} />
+                      <Input
+                        type="text"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                      />
                     </FormControl>
-                    <RadioGroup defaultValue="none" onChange={setShippingOption} value={shippingOption}>
+                    <RadioGroup
+                      mt={5}
+                      defaultValue="none"
+                      onChange={setShippingOption}
+                      value={shippingOption}
+                    >
                       <Stack direction="column">
                         <Radio value="1">Ship to your address</Radio>
                         <Radio value="2">Pick up from the store</Radio>
                       </Stack>
                     </RadioGroup>
                     <FormControl id="Address">
-                      <FormLabel>Please enter your Address</FormLabel>
-                      <Input type="text" onChange={(e)=> setAddress(e.target.value)} value={address} />
+                      <FormLabel mt={5}>Please enter your Address</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => setAddress(e.target.value)}
+                        value={address}
+                      />
                     </FormControl>
                     <FormControl id="City">
-                      <FormLabel>Please enter your City</FormLabel>
-                      <Input type="text" onChange={(e)=> setCity(e.target.value)} value={city} />
+                      <FormLabel mt={5}>Please enter your City</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => setCity(e.target.value)}
+                        value={city}
+                      />
                     </FormControl>
                     <FormControl id="PostalCode">
-                      <FormLabel>Please enter your Postal Code</FormLabel>
-                      <Input type="number" onChange={(e)=> setPostalCode(e.target.value)} value={postalCode} />
+                      <FormLabel mt={5}>
+                        Please enter your Postal Code
+                      </FormLabel>
+                      <Input
+                        type="number"
+                        onChange={(e) => setPostalCode(e.target.value)}
+                        value={postalCode}
+                      />
                     </FormControl>
-                    <RadioGroup defaultValue="none" onChange={setPaymentMethod} value={paymentMethod} >
-                      <Stack direction="column">
+                    <RadioGroup
+                      defaultValue="none"
+                      onChange={setPaymentMethod}
+                      value={paymentMethod}
+                    >
+                      <Stack mt={5} direction="column">
                         <Radio value="1">MercadoPago</Radio>
                         <Radio value="2">Cash</Radio>
                       </Stack>
                     </RadioGroup>
-                    <FormLabel>Any details for delivery?</FormLabel>
-                    <Textarea onChange={(e)=> setComment(e.target.value)} value={comment}/>
+                    <FormLabel mt={5}>Any details for delivery?</FormLabel>
+                    <Textarea
+                      onChange={(e) => setComment(e.target.value)}
+                      value={comment}
+                    />
                   </Box>
                   <DrawerFooter>
                     <Heading as={"h3"} fontSize="2xl" fontWeight="bold">
